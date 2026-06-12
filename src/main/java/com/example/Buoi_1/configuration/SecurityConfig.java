8d48a14ced9a283265f5af7ea43efbab50535965
Nguyễn Anh Tuấn <tuannguyenanh2357@gmail.com>
2026-06-13 00:53:48 +0700

    fix cors for railway

--- src/main/java/com/example/Buoi_1/configuration/SecurityConfig.java
+++ src/main/java/com/example/Buoi_1/configuration/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
